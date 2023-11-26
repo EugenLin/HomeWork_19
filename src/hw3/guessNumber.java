60503f969b84f111cde2aa9dbeee69e91eeb3006
Eugen <eugenlin5555@gmail.com>
2023-11-26 14:40:33 +0800

    hw3 finished

---
+++ src/hw3/guessNumber.java
@@ -0,0 +1,61 @@
+package hw3;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class guessNumber {
+
+	public static void main(String[] args) {
+		System.out.println("Please guess a number between 0~100: ");
+		int answer = answer();
+		int guessed = entryNumber();
+
+		System.out.println("answer =" + answer);
+
+		while (guessed != answer) {
+
+			System.out.println("猜錯囉");
+			guessed = entryNumber();
+
+			if (guessed == answer) {
+				System.out.println("猜對囉");
+				break;
+			}
+		}
+
+	}
+
+//================
+	public static int entryNumber() {
+		System.out.println("Please enter a integar: ");
+
+		int guessed = 0;
+		Scanner sc = new Scanner(System.in);
+		boolean validInput = false;
+
+		while (!validInput) {
+			try {
+				guessed = sc.nextInt();
+				validInput = true;
+//			InputMismatchException e from java.until.InputMismatchException
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid Input. Please enter a valid integer.");
+				sc.next();
+			}
+		}
+
+		return guessed;
+
+	}
+
+//	===========
+	public static int answer() {
+		Random ran = new Random();
+		int answer = ran.nextInt(101);
+		return answer;
+	}
+//	===========
+
+//		================
+}
---
+++ src/hw3/lottery.java
@@ -0,0 +1,48 @@
+package hw3;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class lottery {
+
+	public static void main(String[] args) {
+		System.out.println("阿文...你這次不要哪個數字(0~9):");
+		int banned = banNumber();
+
+		int total5 = 0;
+
+		for (int i5 = 1; i5 <= 49; i5++) {
+
+			int a = i5 % 10;
+			int b = i5 / 10;
+
+			if (a != banned && b != banned) {
+				System.out.print(i5 + "\t");
+				total5 += 1;
+			}
+		}
+		System.out.println();
+		System.out.println("Total " + total5 + " numbers are accetable.");
+
+	}
+
+	public static int banNumber() {
+		int input = 0;
+		Scanner sc = new Scanner(System.in);
+		boolean validInput = false;
+
+		while (!validInput) {
+			try {
+				input = sc.nextInt();
+				validInput = true;
+//			InputMismatchException e from java.until.InputMismatchException
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid Input. Please enter a valid integer.");
+				sc.next();
+			}
+		}
+
+		return input;
+	}
+
+}
